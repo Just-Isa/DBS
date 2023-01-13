57c49fbf19235558011b0da39fb0042436d7aa0e
Isa Kilic <Is4Kilic@hotmail.com>
2023-01-13 20:35:11 +0100

    REFACTOR: Cleanup & ADDITION: Readme added

---
+++ league-dataset-project/README.md
@@ -0,0 +1,11 @@
+
+# Data Science project for analizing the winrate & fairness of Characters in the game League of Legends
+
+
+## Resources 
+
+[League of Legends Dataset](https://www.kaggle.com/code/fernandorubiogarcia/champion-position-win/data)
+
+--- 
+
+## More to see in the ./presentation directory!
--- league-dataset-project/theDeed/winratePerChamp.py
+++ league-dataset-project/theDeed/winratePerChamp.py
@@ -6,13 +6,16 @@ import itertools
 import numpy as np
 
 # -------------------------------------- DETERMINE IF THE MATCHUPS WHERE PUT OUT FAIRLY --------------------------------------
+
 # Check if the Mastery Points of that Character for that user is above 50000
 def main(x):
     return x > 50000
 
+# Check if character is being played for the first time
 def firsTime(x):
     return x < 1000
 
+# Get the champions name
 def getChampionName(x, champsDF:pd.DataFrame):
     return str(champsDF.loc[champsDF['Key'] == x].values[0][0])
 
@@ -30,35 +33,27 @@ result['AmountMatchesLost'] = 0
 result['Winrate'] = 0
 result['PositionPercentage'] = 0
 result['Position'] = ""
-'''
-xd = 0
-for x in gamesDF.values:
-    y = 875
-    if x[3] == y or x[8] == y or x[13] == y or x[18] == y or x[23] == y or x[28] == y or  x[33] == y or x[38] == y or x[43] == y or x[48] == y:
-        print("Sett!")
-        xd = xd+1
-print(xd)
-'''
+
 
 for y in result['Key']:
     champCounter, wonGames, lostGames = 0, 0, 0
     topRate, botRate, midRate, jngRate, supRate = 0, 0, 0, 0, 0
-    for lmao, x in itertools.product(gamesDF.values, range(3, 49, 5)):
-        if lmao[x] == y:
-            if lmao[51] == 't1':
+    for games, x in itertools.product(gamesDF.values, range(3, 49, 5)):
+        if games[x] == y:
+            if games[51] == 't1':
                 wonGames = wonGames + 1
             else:
                 lostGames = lostGames + 1
             champCounter = champCounter + 1
-            if lmao[x-1] == 'TOP':
+            if games[x-1] == 'TOP':
                 topRate = topRate + 1
-            elif lmao[x-1] == 'ADC':
+            elif games[x-1] == 'ADC':
                 botRate = botRate + 1
-            elif lmao[x-1] == 'SUPPORT':
+            elif games[x-1] == 'SUPPORT':
                 supRate = supRate + 1
-            elif lmao[x-1] == 'JUNGLE':
+            elif games[x-1] == 'JUNGLE':
                 jngRate = jngRate + 1
-            elif lmao[x-1] == 'MID':
+            elif games[x-1] == 'MID':
                 midRate = midRate + 1
             
     maxPlayRate = sorted([topRate, botRate, midRate, jngRate, supRate], reverse=True)
